6d449f24fcc2b9841342f7803d89f4df14fc42ac
Ahmed-Zarrad <73597577+Ahmed-Zarrad@users.noreply.github.com>
2023-06-23 04:40:47 +0100

    update

--- frontend/src/app/components/result/result.component.ts
+++ frontend/src/app/components/result/result.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-result',
@@ -6,7 +6,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
-  result: any = null;
+  @Input() image: any;
+  @Input() skinType: any;
+  @Input() probability: any;
+  @Input() symptoms: any;
+  @Input() howCommon: any;
+  @Input() treatments: any;
+  @Input() duration: any;
   constructor(){}
   ngOnInit(): void {}
 }
