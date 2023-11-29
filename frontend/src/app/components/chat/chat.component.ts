a2754ac525c2622c5ff5c06e603863632e92cf4c
Ahmed-Zarrad <73597577+Ahmed-Zarrad@users.noreply.github.com>
2023-11-29 15:12:26 +0100

    update

--- frontend/src/app/components/chat/chat.component.ts
+++ frontend/src/app/components/chat/chat.component.ts
@@ -5,7 +5,8 @@ import { ActivatedRoute} from '@angular/router';
 import {Message} from "../../models/message.model";
 import {User} from "../../models/user.model";
 import {UserService} from "../../services/user/user.service";
-
+import {SkinResultsService} from "../../services/skin-results/skin-results.service";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
@@ -31,7 +32,8 @@ export class ChatComponent implements OnInit {
   loading = false;
   idChat:any;
   result: any;
-  constructor(private route: ActivatedRoute, private chatbotService: ChatbotService, private userService: UserService) { }
+  err: any
+  constructor(private route: ActivatedRoute, private chatbotService: ChatbotService, private userService: UserService, private skinResultsService: SkinResultsService, private router: Router) { }
   ngOnInit() {
   }
   ShowActionMenu() {
@@ -154,7 +156,7 @@ export class ChatComponent implements OnInit {
   sendBotMessage(message:any): void {
     if (message.length === 0) return;
     this.loading = true;
-    const idChat = ""
+    const idChat = this.isChatActive
     this.chatbotService.chatbot(message, 'assistant', idChat).subscribe(
       data => {
         this.resp= data
@@ -188,58 +190,65 @@ export class ChatComponent implements OnInit {
     element.style.height = `${this.initialRowHeight}px`;
     element.style.height = `${element.scrollHeight}px`;
   }
-  // handleImageUpload(event: any):void {
-  //   this.loading = true;
-  //   if (event.target.files.length > 0) {
-  //     const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
-  //
-  //     if (!allowedTypes.includes(event.target.files[0].type)) {
-  //       this.loading = false;
-  //       this.displayBotMessage1(this.msg= 'Invalid image type (only png, jpg, jpeg are allowed).');
-  //     }
-  //
-  //     else if (event.target.files[0].size > 3000000) {
-  //       this.loading = false;
-  //       this.displayBotMessage1(this.msg = 'Image size must be less than 1MB');
-  //     }
-  //
-  //     const skinImage: File = event.target.files[0];
-  //     this.skinResultsService.uploadSkinImage(skinImage).subscribe(
-  //       data => {
-  //         console.log(data);
-  //         this.result = data;
-  //         this.loading = false;
-  //         this.showData = false;
-  //         this.chatbotService.chatbot("the result of processing the user skin image is Skin type: "+this.result.skinType+"Probability: "+this.result.probability+"% ask me 5 yes or no question to verify if he really have this skin type or this skin pathology and Do not ask all the questions at once, send each question separately and after I reply with yes or no ask me the next question until the user answer all the questions after I answer all the question tell me the result of the quiz", 'system').subscribe(
-  //           data => {
-  //             this.resp= data
-  //             console.log(data);
-  //
-  //           },
-  //           error => {
-  //             console.log(error);
-  //             this.msg = 'error';
-  //           }
-  //         )
-  //       },
-  //       error => {
-  //         console.log(error);
-  //         this.err= error;
-  //         this.loading = false;
-  //         if (this.err.status===401) {
-  //           this.displayBotMessage1(this.msg = 'Token expired. You must login to continue');
-  //           setTimeout(() => {
-  //             this.tokenstorageService.logOut();}, 4000);
-  //         }
-  //         else if (this.err.status === 500) {
-  //
-  //           this.displayBotMessage1(this.msg = 'Failed to connect to the server.');
-  //         }
-  //
-  //         else {
-  //           this.displayBotMessage1(this.msg = 'Invalid image.');
-  //         }
-  //       });
-  //   }
-  // }
+  logout() {
+    localStorage.clear();
+    this.router.navigate(["/login"]);
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+  }
+  handleImageUpload(event: any):void {
+    this.loading = true;
+    if (event.target.files.length > 0) {
+      const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+
+      if (!allowedTypes.includes(event.target.files[0].type)) {
+        this.loading = false;
+        this.msg= 'Invalid image type (only png, jpg, jpeg are allowed).';
+      }
+
+      else if (event.target.files[0].size > 3000000) {
+        this.loading = false;
+        this.msg = 'Image size must be less than 1MB';
+      }
+
+      const skinImage: File = event.target.files[0];
+      this.skinResultsService.uploadSkinImage(skinImage).subscribe(
+        data => {
+          console.log(data);
+          this.result = data;
+          this.loading = false;
+          this.showData = false;
+          this.chatbotService.chatbot("the result of processing the user skin image is Skin type: "+this.result.skinType+"Probability: "+this.result.probability+"% ask me 5 yes or no question to verify if he really have this skin type or this skin pathology and Do not ask all the questions at once, send each question separately and after I reply with yes or no ask me the next question until the user answer all the questions after I answer all the question tell me the result of the quiz", 'system', this.c.id).subscribe(
+            data => {
+              this.resp= data
+              console.log(data);
+
+            },
+            error => {
+              console.log(error);
+              this.msg = 'error';
+            }
+          )
+        },
+        error => {
+          console.log(error);
+          this.err= error;
+          this.loading = false;
+          if (this.err.status===401) {
+            this.msg = 'Token expired. You must login to continue';
+            setTimeout(() => {
+              this.logout();}, 4000);
+          }
+          else if (this.err.status === 500) {
+
+            this.msg = 'Failed to connect to the server.';
+          }
+
+          else {
+            this.msg = 'Invalid image.';
+          }
+        });
+    }
+  }
 }
